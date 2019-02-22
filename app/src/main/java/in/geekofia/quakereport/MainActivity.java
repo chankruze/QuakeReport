24f892744aa59071d89c42b70dbff055ac09c565
chankruze <chankruze@gmail.com>
2019-02-22 08:20:19 +0530

    added logic for JSON and date formating

--- app/src/main/java/in/geekofia/quakereport/MainActivity.java
+++ app/src/main/java/in/geekofia/quakereport/MainActivity.java
@@ -14,14 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Create a fake list of earthquake locations.
-        final ArrayList<Quake> quakes = new ArrayList<Quake>();
-        quakes.add(new Quake(7.2,"San Francisco", "Feb 2,2016"));
-        quakes.add(new Quake(6.1,"London", "July 20,2015"));
-        quakes.add(new Quake(3.9,"Tokyo", "Nov 10,2014"));
-        quakes.add(new Quake(5.4,"Mexico City", "May 3,2014"));
-        quakes.add(new Quake(2.8,"Moscow", "Jan 31,2013"));
-        quakes.add(new Quake(4.9,"Rio de Janeiro", "August 19,2012"));
-        quakes.add(new Quake(1.6,"Paris", "Oct 30,2011"));
+        final ArrayList<Quake> quakes = QueryUtils.extractEarthquakes();
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
