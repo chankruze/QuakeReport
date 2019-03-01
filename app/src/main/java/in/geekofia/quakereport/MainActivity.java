bed8eafd0dfaea4c5e91cbb19ee109bf97e6c6bf
chankruze <chankruze@gmail.com>
2019-03-01 08:25:33 +0530

    added retry button, changed default preferences

--- app/src/main/java/in/geekofia/quakereport/MainActivity.java
+++ app/src/main/java/in/geekofia/quakereport/MainActivity.java
@@ -15,7 +15,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -44,6 +46,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
      * TextView that is displayed when the list is empty
      */
     private TextView mEmptyStateTextView;
+    private ProgressBar mLoadingIndicator;
+
+    private Button mRetryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +60,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
         earthquakeListView.setEmptyView(mEmptyStateTextView);
+
+        mRetryButton = findViewById(R.id.retryButton);
+        mLoadingIndicator = findViewById(R.id.loading_indicator);
 
         // Create a new adapter that takes an empty list of earthquakes as input
         mAdapter = new QuakeAdapter(this, new ArrayList<Quake>());
@@ -78,34 +86,42 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             }
         });
 
-        // Get a reference to the ConnectivityManager to check state of network connectivity
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
+        mRetryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mEmptyStateTextView.setText("");
+                mRetryButton.setVisibility(View.GONE);
+                mLoadingIndicator.setVisibility(View.VISIBLE);
+                mRetryButton.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startLoader();
+                    }
+                },1000);
+            }
+        });
 
-        // Get details on the currently active default data network
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        startLoader();
+    }
 
-        // If there is a network connection, fetch data
-        if (networkInfo != null && networkInfo.isConnected()) {
-
-            // Get a reference to the LoaderManager, in order to interact with loaders.
+    private void startLoader() {
+        if (isConnected()) {
+            mLoadingIndicator.setVisibility(View.VISIBLE);
             LoaderManager loaderManager = getLoaderManager();
-
-            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-            // because this activity implements the LoaderCallbacks interface).
             loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
         } else {
-            // Otherwise, display error
-            // First, hide loading indicator so error message will be visible
-            View loadingIndicator = findViewById(R.id.loading_indicator);
-            loadingIndicator.setVisibility(View.GONE);
-
-            // Update empty state with no connection error message
+            mLoadingIndicator.setVisibility(View.GONE);
             mEmptyStateTextView.setText(R.string.no_internet_connection);
+            mRetryButton.setVisibility(View.VISIBLE);
         }
     }
 
+    private boolean isConnected() {
+        // Check for connectivity status
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connMgr.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
 
     @Override
     public Loader<ArrayList<Quake>> onCreateLoader(int i, Bundle bundle) {
